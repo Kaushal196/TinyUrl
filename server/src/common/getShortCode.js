938d88801287a543375f8986d7282339e631a60e
Kaushal196 <pandeykaushal847@gmail.com>
2024-08-02 19:16:57 +0530

    Added unique code generate logic

--- server/src/common/getShortCode.js
+++ server/src/common/getShortCode.js
@@ -1,13 +1,19 @@
+const pool = require('../../database')
 const ALPHABET = 'aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789'
 
-function getShortCode() {
+async function getShortCode() {
   const size = ALPHABET.length
-  let shortUrl = ''
-  for(let i=0; i<7; i++) {
-    const randomIndex = Math.floor(Math.random() * size) + 1
-    shortUrl += ALPHABET[randomIndex]
+  while(true) {
+    let shortCode = ''
+    for(let i=0; i<7; i++) {
+      const randomIndex = Math.floor(Math.random() * size)
+      shortCode += ALPHABET[randomIndex]
+    }
+    const { rows } = await pool.query('SELECT * FROM urls WHERE short_url = $1;', [shortCode]);
+    if (rows.length === 0) {
+      return shortCode
+    }
   }
-  return shortUrl
 }
 
 module.exports = getShortCode
